210d33c05c522b3fd9c5de7de8f40ab01f9c8a06
Raybrilliant <r.f.brilliansyach@gmail.com>
2025-06-24 16:55:02 +0700

    fix :homepage

---
+++ resources/js/components/localDatabase.js
@@ -0,0 +1,65 @@
+// Informasi PPDB
+export const ppdb = {
+    name: 'PPDB Gelombang 2',
+    date: '1 Februari - 15 Februari 2025',
+    message: 'Penerimaan Peserta Didik Baru (PPDB) MAN 2 Kota Probolinggo Tahun Ajaran 2025/2026',
+    image: '/home/ppdb.jpg',
+}
+
+// Testimoni
+export const testimoni = {
+    name: 'Aisyah Nuril',
+    image:'/home/testimoni1.png',
+    message: 'Sekolah di MAN 2 Kota Probolinggo memberikan pengalaman yang luar biasa, saya sangat menyarankan sekolah ini.',
+}
+
+// Gambar Homepage
+export const gambarhomepage = [
+    {
+        image: '/home/bg-1.jpg',
+    },
+    {
+        image: '/home/bg-2.jpg',
+    },
+]
+
+// Program Unggulan
+export const programs = [
+    {
+        name: 'Peserta Didik Cerdas Istimewa',
+        icon:'/logo/pdcibi.png',
+        message: 'kurikulum bagi peserta didik yang memenuhi syarat untuk mempersingkat waktu peserta didik dalam proses belajar mengajar menjadi 2 tahun',
+    },
+    {
+        name: 'Tahfidz Al-Quran',
+        icon:'/logo/tahfidz.png',
+        message: 'didukung dengan beberapa metode dalam menghafal Al-Quran dan guru pendamping agar memperlancar dan mempermudah siswa dalam proses pembelajaran tahfidz',
+    },
+    {
+        name: 'MA Plus Keterampilan',
+        icon:'/logo/ma_plus.png',
+        message: "program multimedia, robotik, animasi, film, desain grafis, DKV, aplikasi perkantoran, RPL, TKJ, dan operator komputer. ",
+    },
+    {
+        name: 'Program Madrasah Riset',
+        icon:'/logo/research.png',
+        message: 'budaya akademik berbasis riset dan menghasilkan riset yang bermanfaat untuk pengembangan ilmu pengetahuan dan teknologi.',
+    },
+    {
+        name: 'Kelas Ilmu Bahasa dan Budaya',
+        icon:'/logo/ibb.png',
+        message: 'bertujuan untuk mengembangkan pemahaman dan keterampilan dalam bidang bahasa Jepang dan Arab.',
+    },
+    {
+        name: 'Kelas Ilmu Keagamaan',
+        icon:'/logo/iik.png',
+        message: 'bertujuan untuk meningkatkan pemahaman, penghayatan, dan pengamalan peserta didik tentang agama',
+    },
+]
+
+// Kepala Madrasah
+export const principle = {
+    name: 'Drs. H. Moh. Alfan Makmur, M.M',
+    image: '/home/principle.png',
+    message: "Di Madrasah Aliyah Negeri 2 Kota Probolinggo, kami percaya bahwa pendidikan adalah kunci untuk membuka masa depan yang lebih cerah. Kami bangga telah membantu banyak lulusan kami menembus perguruan tinggi favorit, baik di dalam maupun luar negeri, serta meraih prestasi yang membanggakan di berbagai bidang. <br /> <br /> Dengan pendekatan pendidikan yang menyeluruh—memadukan kecerdasan akademis dan penguatan karakter berbasis nilai-nilai Islam—kami berkomitmen untuk terus mencetak generasi yang siap menghadapi tantangan dunia global. Kami percaya bahwa setiap siswa memiliki potensi luar biasa yang perlu dibimbing dan dikembangkan. <br /> <br /> Semoga perjalanan pendidikan yang akan dihadapi putra-putri Anda di sini menjadi langkah menuju kesuksesan mereka.",
+}
--- resources/js/pages/home.jsx
+++ resources/js/pages/home.jsx
@@ -1,53 +1,6 @@
 import { Link } from "@inertiajs/react";
 import HomeLayout from "@/layouts/home-layout";
-
-// Testimoni
-const testimoni = {
-    name: 'Aisyah Nuril',
-    image:'/home/testimoni1.png',
-    message: 'Sekolah di MAN 2 Kota Probolinggo memberikan pengalaman yang luar biasa, saya sangat menyarankan sekolah ini.',
-}
-
-// Program Unggulan
-const programs = [
-    {
-        name: 'Peserta Didik Cerdas Istimewa',
-        icon:'/logo/pdcibi.png',
-        message: 'kurikulum bagi peserta didik yang memenuhi syarat untuk mempersingkat waktu peserta didik dalam proses belajar mengajar menjadi 2 tahun',
-    },
-    {
-        name: 'Tahfidz Al-Quran',
-        icon:'/logo/tahfidz.png',
-        message: 'didukung dengan beberapa metode dalam menghafal Al-Quran dan guru pendamping agar memperlancar dan mempermudah siswa dalam proses pembelajaran tahfidz',
-    },
-    {
-        name: 'MA Plus Keterampilan',
-        icon:'/logo/ma_plus.png',
-        message: "program multimedia, robotik, animasi, film, desain grafis, DKV, aplikasi perkantoran, RPL, TKJ, dan operator komputer. ",
-    },
-    {
-        name: 'Program Madrasah Riset',
-        icon:'/logo/research.png',
-        message: 'budaya akademik berbasis riset dan menghasilkan riset yang bermanfaat untuk pengembangan ilmu pengetahuan dan teknologi.',
-    },
-    {
-        name: 'Kelas Ilmu Bahasa dan Budaya',
-        icon:'/logo/ibb.png',
-        message: 'bertujuan untuk mengembangkan pemahaman dan keterampilan dalam bidang bahasa Jepang dan Arab.',
-    },
-    {
-        name: 'Kelas Ilmu Keagamaan',
-        icon:'/logo/iik.png',
-        message: 'bertujuan untuk meningkatkan pemahaman, penghayatan, dan pengamalan peserta didik tentang agama',
-    },
-]
-
-// Kepala Madrasah
-const principle = {
-    name: 'Drs. H. Moh. Alfan Makmur, M.M',
-    image: '/home/principle.png',
-    message: "Di Madrasah Aliyah Negeri 2 Kota Probolinggo, kami percaya bahwa pendidikan adalah kunci untuk membuka masa depan yang lebih cerah. Kami bangga telah membantu banyak lulusan kami menembus perguruan tinggi favorit, baik di dalam maupun luar negeri, serta meraih prestasi yang membanggakan di berbagai bidang. <br /> <br /> Dengan pendekatan pendidikan yang menyeluruh—memadukan kecerdasan akademis dan penguatan karakter berbasis nilai-nilai Islam—kami berkomitmen untuk terus mencetak generasi yang siap menghadapi tantangan dunia global. Kami percaya bahwa setiap siswa memiliki potensi luar biasa yang perlu dibimbing dan dikembangkan. <br /> <br /> Semoga perjalanan pendidikan yang akan dihadapi putra-putri Anda di sini menjadi langkah menuju kesuksesan mereka.",
-}
+import { ppdb, testimoni, gambarhomepage, programs, principle } from "@/components/localDatabase";
 
 function Home({faqs,user}) {
     return (
@@ -79,11 +32,11 @@ function Home({faqs,user}) {
                         <img src="/home/bg-register.jpg" alt="PPDB" className='object-cover w-full h-full blur-[1px] object-center' />
                         <div className="absolute inset-0 bg-black opacity-50"></div>
                         <div className="absolute inset-0 flex text-white flex-col p-5 uppercase font-bold text-4xl">
-                            <h1>PPDB Gelombang 2</h1>
-                            <p>1 Februari - 15 Februari 2025</p>
+                            <h1>{ppdb.name}</h1>
+                            <p>{ppdb.date}</p>
                         </div>
                         <div className="absolute inset-0 flex top-auto bottom-0 text-white flex-col p-5 font-bold text-3xl">
-                            <p>Penerimaan Peserta Didik Baru (PPDB) MAN 2 Kota Probolinggo Tahun Ajaran 2025/2026</p>
+                            <p>{ppdb.message}</p>
                         </div>
                     </div>
                     {/* Right */}
@@ -99,8 +52,9 @@ function Home({faqs,user}) {
                         </div>
                         {/* Right Bottom */}
                         <div className='flex gap-2'>
-                            <img src="/home/bg-1.jpg" alt="ppdb" className='w-full aspect-video object-cover rounded-xl' />
-                            <img src="/home/bg-2.jpg" alt="ppdb" className='w-full aspect-video object-cover object-center rounded-xl' />
+                            {gambarhomepage.map((gambar, index) => (
+                                <img key={index} src={gambar.image} alt="ppdb" className='w-full aspect-video object-cover rounded-xl' />
+                            ))}
                         </div>
                     </div>
                 </div>
